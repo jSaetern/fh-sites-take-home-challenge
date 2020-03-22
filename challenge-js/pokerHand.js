2fbedcf6d7132e6a55e594cdaa8c5881c27e8b9a
jsaetern <jSaetern@users.noreply.github.com>
2020-03-22 11:01:18 -0600

    Evaluate

--- challenge-js/pokerHand.js
+++ challenge-js/pokerHand.js
@@ -1,9 +1,130 @@
 class PokerHand {
-  constructor() {}
+  constructor(hand) {
+    this.theHand = hand;
+  }
 
+  // Implement poker hand ranking
   getRank() {
-    // Implement poker hand ranking
-    return 'Royal Flush';
+    const ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"];
+    const suits = ["c", "d", "h", "s"];
+    let rankArray = [];
+    let suitArray = [];
+
+    var handGiven = this.theHand;
+    let arrayHandGiven = handGiven.split(" ");
+
+    // Sort the rank from lowest to highest
+    function sorted() { 
+      let sortedHand = [];
+      for (let i = 0; i < ranks.length; i++) {
+        for (let j = 0; j < arrayHandGiven.length; j++) {
+          if (ranks[i] === arrayHandGiven[j].charAt(0)) {
+            sortedHand.push(arrayHandGiven[j]);
+          }
+        }
+      }
+      return sortedHand;
+    }
+
+    let sortedHandGiven = sorted(arrayHandGiven);
+    
+    // Push the sorted ranks and suits into their empty arrays
+    function addRankSuit(sortedHandGiven) {
+      for (let i = 0; i < sortedHandGiven.length; i++) {
+        let card = sortedHandGiven;
+        rankArray.push(card[i].charAt(0));
+        suitArray.push(card[i].charAt(1));
+      }
+    }
+
+    addRankSuit(sortedHandGiven);
+  
+    // Count the number of ranks that are the same from ranks array
+    function countRanks(rankArray) { 
+      let rankCount = {};
+      rankArray.forEach(function(x) {
+        rankCount[x] = (rankCount[x] || 0) + 1;
+      });
+      return rankCount;
+    }
+
+    // Count the number of suits that are the same from suits array
+    function countSuits(suitArray) {
+      let suitCount = {};
+      suitArray.forEach(function(x) {
+        suitCount[x] = (suitCount[x] || 0) + 1;
+      });
+      return suitCount;
+    }    
+
+    // Check the numerical order of the ranks array
+    function isStraight() { 
+      let index = ranks.indexOf(rankArray[0]);
+      let ref = ranks.slice(index, index + 5).join("");
+      let section = rankArray.slice(0).join("");
+      if (section === "TJQKA" && section === ref) {
+        return "royal";
+      } else if (section === "2345A" || section === ref) {
+        return "straight";
+      } else {
+        return "false";
+      }
+    }
+
+    // Check if all suits are the same in the suits array
+    function isFlush() { 
+      let cS = countSuits(suitArray);
+      if (Object.keys(cS).find(key => cS[key] === 5)) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  
+    // Check for pair instances in the ranks array
+    function pairs() { 
+      let rS = countRanks(rankArray);
+      return Object.keys(rS).filter(key => rS[key] === 2).length;
+    }
+  
+    // Evaluate and determine the hand ranking
+    function evaluateHand() {
+      let rS = countRanks(rankArray);
+      if (isFlush() === true && isStraight() === "royal") {
+        var royal = "Royal Flush";
+        return royal;
+      } else if (isFlush() === true && isStraight() === "straight") {
+        var str = "Straight Flush";
+        return str;
+      } else if (Object.keys(rS).find(key => rS[key] === 4)) {
+        var four = "Four of a Kind";
+        return four;
+      } else if (Object.keys(rS).find(key => rS[key] === 3) && pairs() === 1) {
+        var fullhouse = "Full House";
+        return fullhouse;
+      } else if (isFlush() === true) {
+        var flush = "Flush";
+        return flush;
+      } else if (isStraight() === "straight") {
+        var straight = "Straight";
+        return straight;
+      } else if (Object.keys(rS).find(key => rS[key] === 3)) {
+        var three = "Three of a Kind";
+        return three;
+      } else if (pairs() === 2) {
+        var two = "Two Pair";
+        return two;
+      } else if (pairs() === 1) {
+        var one = "One Pair";
+        return one;
+      } else {
+        var high = "High Card";
+        return high;
+      }
+    }
+
+    return evaluateHand();
+
   }
 }
 
--- challenge-js/test/pokerHandTest.js
+++ challenge-js/test/pokerHandTest.js
@@ -6,7 +6,7 @@ var PokerHand = require('../pokerHand.js');
  */
 describe('Rank a Royal Flush', function() {
   it('Return royal flush when hand given', function() {
-    var hand = new PokerHand('As Ks Qs Js 10s');
+    var hand = new PokerHand('As Ks Qs Js Ts');
     assert.equal(hand.getRank(), 'Royal Flush');
   });
 });
@@ -25,7 +25,7 @@ describe('Rank a Pair', function() {
 /**
  * test
  */
-describe('Rank Two Pair', function() {
+describe('Rank a Two Pair', function() {
   it('Return two pair when hand given', function() {
     var hand = new PokerHand('Kh Kc 3s 3h 2d');
 
@@ -36,7 +36,7 @@ describe('Rank Two Pair', function() {
 /**
  * test
  */
-describe('Rank A Flush', function() {
+describe('Rank a Flush', function() {
   var hand = new PokerHand('Kh Qh 6h 2h 9h');
 
   it('Return flush when hand given', function() {
@@ -45,3 +45,60 @@ describe('Rank A Flush', function() {
 });
 
 // More tests go here
+
+describe('Rank a Straight Flush', function() {
+  it('Return straight flush when hand given', function() {
+    var hand = new PokerHand('9s 8s 7s 6s 5s');
+    assert.equal(hand.getRank(), 'Straight Flush');
+  });
+});
+
+describe('Rank a Four of a Kind', function() {
+  it('Return four of a kind when hand given', function() {
+    var hand = new PokerHand('Ac Ad Ah As 2c');
+    assert.equal(hand.getRank(), 'Four of a Kind');
+  });
+});
+
+describe('Rank a Full House', function() {
+  it('Return full house when hand given', function() {
+    var hand = new PokerHand('Ac Ad Ah Ks Kc');
+    assert.equal(hand.getRank(), 'Full House');
+  });
+});
+
+describe('Rank a Straight', function() {
+  it('Return straight when hand given', function() {
+    var hand = new PokerHand('Ac 2s 3s 4s 5s');
+    assert.equal(hand.getRank(), 'Straight');
+  });
+});
+
+describe('Rank a Three of a Kind', function() {
+  it('Return three of a kind when hand given', function() {
+    var hand = new PokerHand('9s 9c 9d 2s 5s');
+    assert.equal(hand.getRank(), 'Three of a Kind');
+  });
+});
+
+describe('Rank a High Card', function() {
+  it('Return high card when hand given', function() {
+    var hand = new PokerHand('2s 4c 6d 8h 9s');
+    assert.equal(hand.getRank(), 'High Card');
+  });
+});
+
+
+
+
+
+
+
+/**
+
+PSEUDO CODE & NOTES SECTION:
+-Build function that generates random 5-card hand of ranks and suits
+-Duplicates of the same card should be invalid
+-More than 4 ranks of the same value should be invalid
+
+**/
